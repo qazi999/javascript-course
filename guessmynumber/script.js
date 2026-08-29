e62b52d4252d4b1d9f820311facef6c9e66317e7
Qazi Hamzah <QAZIHAMZAH7@GAMIL.COM>
2026-08-29 12:34:52 +0530

    Working with modals

--- guessmynumber/script.js
+++ guessmynumber/script.js
@@ -23,7 +23,7 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
-  // When there is no input
+  // When there is no input 
   if (!guess) {
     // document.querySelector('.message').textContent = '⛔️ No number!';
     displayMessage('⛔️ No number!');
